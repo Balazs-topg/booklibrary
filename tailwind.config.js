a47603c3f3e62d24ea76d404a6556b0346df6814
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-02-24 18:32:59 +0100

    install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"], //consider linking js file aswell
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
