a26539275d10d858daa4370b0054c2525ef77859
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-02-26 09:11:06 +0100

    Move some files around

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"], //consider linking js file aswell
+  content: [".src/**/*{html,js}"], //consider linking js file aswell
   theme: {
     extend: {},
   },
